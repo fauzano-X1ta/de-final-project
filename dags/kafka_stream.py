b7d56d8f214f6bd1cd7f4be790779ba1930a170f
zano <fauzano6512@gmail.com>
2024-04-20 12:00:09 +0700

    Sukses Produce Kafka

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -36,26 +36,31 @@ def format_data(res):
     data['registered_date'] = res['registered']['date']
     data['phone'] = res['phone']
     data['nat'] = res['nat']
-    data['email']
+    data['email'] = res['email']
 
     return  data
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
+
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent=3))
+    #print(json.dumps(res, indent=3))
 
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
 
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
 
-# with DAG('user_automation',
-#          default_args=default_args,
-#          schedule_interval='@daily',
-#          catchup=False) as dag:
-#
-#     streaming_task = PythonOperator(
-#         task_id='stream_data_from_api',
-#         python_callable=stream_data
-#     )
+with DAG('user_automation',
+           default_args=default_args,
+           schedule_interval='@daily',
+           catchup=False) as dag:
+
+      streaming_task = PythonOperator(
+          task_id='stream_data_from_api',
+          python_callable=stream_data
+      )
 
 stream_data()
