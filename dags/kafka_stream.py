7c90dc69861c67c1d53ff0702d547b75bc632b9c
zano <fauzano6512@gmail.com>
2024-04-20 12:14:27 +0700

    Updating kafka_stram.py sesuai agar bisa dipakai di airflow

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -44,14 +44,23 @@ def stream_data():
     import json
     from kafka import KafkaProducer
     import time
-
-    res = get_data()
-    res = format_data(res)
-    #print(json.dumps(res, indent=3))
+    import logging
 
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    while True:
+        if time.time() > curr_time +60:    #1 menit
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+
 
 with DAG('user_automation',
            default_args=default_args,
@@ -63,4 +72,4 @@ with DAG('user_automation',
           python_callable=stream_data
       )
 
-stream_data()
+#stream_data()
